1eceb92655bbd7e4fb8e28f1db7c6c65a935cb1f
David Chu <dchu26@ucmerced.edu>
2022-03-01 00:05:34 -0800

    Finished SimpleTweet's basic requirements

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -5,10 +5,12 @@ import com.codepath.apps.restclienttemplate.TimeFormatter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
 
     public String body;
@@ -16,6 +18,10 @@ public class Tweet {
     public long id;
     public User user;
     public String timestamp;
+
+    public Tweet(){
+
+    }
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
